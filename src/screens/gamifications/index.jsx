cbd3df745ad7bd4ef95273d2a6408a79c4659e12
Fernando Llopis <fllopis.tarraga@gmail.com>
2024-09-19 21:51:02 +0200

    Starting & bottom menu

---
+++ src/screens/gamifications/index.jsx
@@ -0,0 +1,17 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+
+/**
+ * Function that return the JSX for GamificationsScreen
+ * @returns <GamificationsScreen />
+ */
+const GamificationsScreen = () => {
+  return (
+    <View>
+      <Text>GAMIFICATIONS</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+
+export default GamificationsScreen;
