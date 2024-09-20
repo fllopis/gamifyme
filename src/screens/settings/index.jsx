ccbdfd7241cf966e914916d26316b3aab803eda4
Fernando Llopis <fllopis.tarraga@gmail.com>
2024-09-20 02:01:28 +0200

    Redux to control theme of application

--- src/screens/settings/index.jsx
+++ src/screens/settings/index.jsx
@@ -1,17 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import { useSelector } from "react-redux";
+
+//Utils
+import { getThemeColor } from "../../utils/utils";
 
 /**
  * Function that return the JSX for SettingsScreen
  * @returns <SettingsScreen />
  */
 const SettingsScreen = () => {
+  const theme = useSelector((state) => state.theme.theme);
+  const themeColor = getThemeColor({ theme });
+
   return (
-    <View>
-      <Text>HISTORY</Text>
-      <StatusBar style="auto" />
+    <View style={[styles.container, { backgroundColor: themeColor }]}>
+      <Text>SETTINGS - {theme}</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
 export default SettingsScreen;
